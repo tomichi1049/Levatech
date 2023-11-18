0f111f15baa77d81f472005c7c3994ad7910393c
tomichi1049 <tomitachisato8025@gmail.com>
2023-11-19 08:58:00 +0900

    2023/11/19 8:57 commit

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
 
 Route::get('/posts/{post}', [PostController::class ,'show']);
