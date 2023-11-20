4852da598635d51383f59e8ebef033deb16ddc10
tomichi1049 <tomitachisato8025@gmail.com>
2023-11-20 11:37:44 +0900

    2023/11/20 11:37 commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Models;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use App\Models\Post;
 
-class Post extends Model
+class PostController extends Controller
 {
-    use HasFactory;
+    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    {
+        return $post->get();//$postの中身を戻り値にする。
+    }
 }
