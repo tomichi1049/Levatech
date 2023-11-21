44120ab97b7bdf88c2cf2127e4fd3a4c8ec5b221
tomichi1049 <tomitachisato8025@gmail.com>
2023-11-21 16:27:27 +0900

    2023/11/21 16:27 commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    
+    /**
+     * 特定IDのpostを表示する
+     *
+     * @params Object Post // 引数の$postはid=1のPostインスタンス
+     * @return Reposnse post view
+     */
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+}
+?>
