00a346f559b9420365b8b1c4ba132e02c1d9d170
tomichi1049 <tomitachisato8025@gmail.com>
2023-11-22 16:40:24 +0900

    2023/11/22 16:40 commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
@@ -11,31 +11,22 @@ class PostController extends Controller
     {
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
-    
-    /**
-     * 特定IDのpostを表示する
-     *
-     * @params Object Post // 引数の$postはid=1のPostインスタンス
-     * @return Reposnse post view
-     */
+
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
-     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
-    
+
     public function create()
     {
         return view('posts.create');
-        
     }
-    
-    public function store(Request $request, Post $post)
+
+    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
     {
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
-    
 }
 ?>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
-<html lang="{{str_replace('_','-',app()->getLocale())}}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf=8">
+        <meta charset="utf-8">
         <title>Blog</title>
     </head>
     <body>
@@ -9,17 +9,17 @@
         <form action="/posts" method="POST">
             @csrf
             <div class="title">
-                <h2>Title</h2><br>
-                <input type="text" name="post[title]" placeholder="タイトル/">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れ様でした。"></textarea>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old('post.body') }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
-            <input type="submit" value="store">
+            <input type="submit" value="保存"/>
         </form>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
+        <div class="back">[<a href="/">back</a>]</div>
     </body>
 </html>
