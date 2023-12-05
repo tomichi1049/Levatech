fae329e5d99fa54d49455f6ef3fe90fb4d065263
tomichi1049 <tomitachisato8025@gmail.com>
2023-12-05 18:31:28 +0900

    2023/12/05 18:31 commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,11 +17,11 @@ class PostController extends Controller
         return view('posts.show')->with(['post' => $post]);
     }
 
-    public function create()
+    public function create(Category $category)
     {
-        return view('posts.create');
+        return view('posts.create')->with(['categories'=>$category->get()]);
     }
-
+    
     public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
     {
         $input = $request['post'];
