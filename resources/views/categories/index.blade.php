bcc3963e6c29c767e3eca2da8d69ec0f7248d27e
tomichi1049 <tomitachisato8025@gmail.com>
2023-12-09 23:37:27 +0900

    2023/12/09 23:37 commit

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -15,7 +15,6 @@
                     <h2 class='title'>
                         <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                     </h2>
-                    <a href="">{{ $post->category->name }}</a>
                     <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
                     <p class='body'>{{ $post->body }}</p>
                     <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
